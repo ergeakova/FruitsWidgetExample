bb0847a6626d3573adcad69962c7136e6583b086
ergeakova <ergeak@gmail.com>
2022-07-06 01:16:31 +0300

    Initial Commit

---
+++ FruitsWidgetExample/FruitsWidgetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsWidgetExampleApp.swift
+//  FruitsWidgetExample
+//
+//  Created by Erge Gevher Akova on 6.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsWidgetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
